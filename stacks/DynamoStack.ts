dea1adb5b0e00469b9944814f22c69509942d3e5
bryxli <bryanzero765@gmail.com>
2025-04-05 11:36:49 -0500

    Rename table to users

--- stacks/DynamoStack.ts
+++ stacks/DynamoStack.ts
@@ -1,12 +1,12 @@
 import { StackContext, Table } from "sst/constructs";
 
 export function Dynamo({ stack }: StackContext) {
-  const table = new Table(stack, "db", {
+  const users = new Table(stack, "users", {
     fields: {
       user_id: "string",
     },
     primaryIndex: { partitionKey: "user_id" },
   });
 
-  return { table };
+  return { users };
 }
